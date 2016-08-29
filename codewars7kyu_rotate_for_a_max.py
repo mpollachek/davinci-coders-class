2c5a47dab708c8f0f088802bb20bb32c8d5b7cd8
mpollachek <mpollachek81@gmail.com>
2016-08-29 00:55:21 -0600

    Add files via upload

---
+++ codewars7kyu_rotate_for_a_max.py
@@ -0,0 +1,32 @@
+Take a number: 56789. Rotate left, you get 67895.
+
+Keep the first digit in place and rotate left the other digits: 68957.
+
+Keep the first two digits in place and rotate the other ones: 68579.
+
+Keep the first three digits and rotate left the rest: 68597. Now it is over since keeping the first four it remains only one digit which rotated is itself.
+
+You have the following sequence of numbers:
+
+56789 -> 67895 -> 68957 -> 68579 -> 68597
+
+and you must return the greatest: 68957.
+
+Calling this function max_rot (or maxRot or ... depending on the language)
+
+max_rot(56789) should return 68957
+
+
+
+def max_rot(n):
+    a = list(str(n))
+    b = [n]
+    index = 0
+    end = len(a) - 1
+    while index <= end - 1:
+        a.insert(end, a.pop(index))
+        y = ''.join(map(str, a))
+        b.insert(-1, y)
+        index +=1
+    return max(b)
+
